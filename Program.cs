9370c63ebaab52554d72e8f8c9b428cb807dab19
Maciej Pastuszak <maciek.pastuszak@gmail.com>
2021-07-21 23:00:49 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Maciej Pastuszak";
+            Console.WriteLine(name);
+        }
+    }
+}
