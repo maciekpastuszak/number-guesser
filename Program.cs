ddaf83b568043abf2f67ef6eef167856f270f21e
Maciej Pastuszak <maciek.pastuszak@gmail.com>
2021-07-22 00:10:07 +0200

    add greeting

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace NumberGuesser
             int appVersion = 1;
             string appAuthor = "Maciej Pastuszak";
 
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
             Console.WriteLine($"[{appName}] Wersja: 0.0.{appVersion}, Autor: {appAuthor}");
+
+            Console.ResetColor();
+
+            Console.WriteLine("Jak masz na imię?");
+
+            string inputUserName = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine($"Powodzenia {inputUserName}, odgadnij liczbę ...");
+
+            Console.ResetColor();        
+        
         }
+
     }
 }
